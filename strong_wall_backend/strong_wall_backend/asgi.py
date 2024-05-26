21f876f5c02bd98a16fd00be605b0ce631f8691d
mhvozdesky <m.hvozdesky@gmail.com>
2024-05-26 15:58:25 +0300

    start backend

---
+++ strong_wall_backend/strong_wall_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for strong_wall_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strong_wall_backend.settings')
+
+application = get_asgi_application()
