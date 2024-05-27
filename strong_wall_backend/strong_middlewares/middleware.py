a22d74817b28c04b0aee30d721b1cbb6c92a3e2c
mhvozdesky <m.hvozdesky@gmail.com>
2024-05-27 12:39:35 +0300

    added blocking of the ip address if the request limit is exceeded

--- strong_wall_backend/strong_middlewares/middleware.py
+++ strong_wall_backend/strong_middlewares/middleware.py
@@ -5,13 +5,26 @@ from django.http import JsonResponse
 
 
 class RateLimitMiddleware:
+    """
+    RateLimitMiddleware provides basic protection against DDoS attacks by
+    limiting the number of requests from one IP address during a given period
+    of time. If the number of requests exceeds the limit normal, the IP address
+    is blocked for a certain time.
+    """
     def __init__(self, get_response):
         self.get_response = get_response
         self.rate_limit = settings.RATE_LIMIT
 
     def __call__(self, request):
         ip = self.get_client_ip(request)
+
+        # Checking if the IP is blocked
+        if self.is_blocked(ip):
+            return JsonResponse({"error": "IP temporarily blocked"}, status=403)
+
+        # Checking the request limit
         if self.is_rate_limited(ip):
+            self.block_ip(ip)
             return JsonResponse({"error": "Too many requests"}, status=429)
 
         response = self.get_response(request)
@@ -36,7 +49,22 @@ class RateLimitMiddleware:
                              "TIME_PERIOD"]]
         request_times.append(current_time)
 
+        # Storing keys with a timeout equal to the time period for restricting requests
         cache.set(cache_key, request_times,
                   timeout=self.rate_limit["TIME_PERIOD"])
 
+        # Login to verify installation of keys
+        print(f"Set cache key {cache_key} with values {request_times}")
+
         return len(request_times) > self.rate_limit["MAX_REQUESTS"]
+
+    def block_ip(self, ip):
+        # Block the IP for a certain time
+        block_time = self.rate_limit.get("BLOCK_TIME", 600)
+        cache.set(f"blocked_{ip}", True, timeout=block_time)
+
+        # Additional logging
+        print(f"Blocked IP {ip} for {block_time} seconds")
+
+    def is_blocked(self, ip):
+        return cache.get(f"blocked_{ip}", False)
